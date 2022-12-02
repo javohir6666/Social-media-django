1122a130a9ce5767fc5d47671ae076ba9d4365ab
javohir6666 <javohir6666@gmail.com>
2022-12-02 15:46:59 +0500

    comment added

--- social/models.py
+++ social/models.py
@@ -6,3 +6,8 @@ class Post(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+class Comment(models.Model):
+    comment = models.CharField(max_length=500)
+    created_at= models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey('Post', on_delete=models.CASCADE)
--- social/views.py
+++ social/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .models import Post
+from django.urls import reverse_lazy
+from .models import Post, Comment
 from django.views import View
-from .forms import PostForm
+from django.views.generic.edit import UpdateView, DeleteView
+from .forms import PostForm, CommentForm
 class PostListView(View):
     def get(self,request, *args, **kwargs):
-        posts = Post.objects.all().order_by('created_at')
+        posts = Post.objects.all().order_by('-created_at')
         form = PostForm()
         context = {
             'post_list':posts,
@@ -27,3 +29,54 @@ class PostListView(View):
         }
         
         return render(request, 'social/post_list.html', context)
+    
+class PostDetailView(View):
+    def get(self,request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        form = CommentForm()
+        comments = Comment.objects.filter(post=post).order_by('-created_at')
+        context = {
+            'post':post,
+            'form': form,
+            'comments':comments
+        }
+        return render(request, 'social/post_detail.html', context)
+    
+    def post(self,request, pk, *args, **kwargs):
+        post = Post.objects.get(pk=pk)
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            new_comment = form.save(commit=False)
+            new_comment.author = request.user
+            new_comment.post = post
+            new_comment.save()
+        comments = Comment.objects.filter(post=post).order_by('-created_at')
+            
+        context = {
+            'post':post,
+            'form': form,
+            'comments':comments
+        }
+        return render(request, 'social/post_detail.html', context)
+    
+
+class PostEditView(UpdateView):
+    model = Post
+    fields = ['body']
+    template_name = 'social/post_edit.html'
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('post-detail', kwargs = {'pk':pk})
+    
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'social/post_delete.html'
+    success_url = reverse_lazy('post-list')
+
+class CommentDeleteView(DeleteView):
+    model = Comment
+    template_name = 'social/comment_delete.html'
+    
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+        return reverse_lazy('post-detail', kwargs ={'pk':pk})
