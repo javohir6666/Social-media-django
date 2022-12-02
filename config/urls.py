4fc2d7d18bfe5f48d5f23d3dea2169a57b98da18
javohir6666 <javohir6666@gmail.com>
2022-12-02 15:48:12 +0500

    first commit

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from landing.views import Index
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('landing.urls')),
